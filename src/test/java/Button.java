b60a31a34fc1ef697e9c9dfcf0c0a2a9ff0e1916
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-25 21:14:44 +0530

    add button testcase

--- src/test/java/Button.java
+++ src/test/java/Button.java
@@ -1,6 +1,7 @@
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Button {
 
@@ -9,10 +10,48 @@ public class Button {
     @BeforeMethod
     public void openLinkTestPage(){
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        // Open Browser
+        Dimension newSize = new Dimension(800, 600);
+        driver.manage().window().setSize(newSize);
+        //driver.manage().window().maximize();
+             // Open Browser
         driver.get("https://www.leafground.com/button.xhtml");
     }
 
+    @Test
 
+            public void buttonTest(){
+
+        // Click and Confirm title
+        driver.findElement(By.xpath("//button[@id='j_idt88:j_idt90']")).click();
+        String expectedTitle = "Dashboard";
+        String actualTitle = driver.getTitle();
+
+        if(expectedTitle.equals(actualTitle)){
+            System.out.println("Actual Title same as expected");
+        } else {
+            System.out.println("Actual Title not same as expected");
+        }
+
+        // Find the position of the submit button
+        driver.navigate().back();
+
+        WebElement getPosition = driver.findElement(By.id("j_idt88:j_idt94"));
+        Point xypoints = getPosition.getLocation();
+        int x = xypoints.getX();
+        int y = xypoints.getY();
+        System.out.println("X position is : " + x + " Y position is : " + y );
+
+        // Find the save button color
+        WebElement buttonColor = driver.findElement(By.id("j_idt88:j_idt96"));
+        String color = buttonColor.getCssValue("background-color");
+        System.out.println("button Color is : " + color);
+
+        // Find the height and width of this button
+        WebElement size = driver.findElement(By.id("j_idt88:j_idt98"));
+        int height = size.getSize().getHeight();
+        int width = size.getSize().getWidth();
+        System.out.println("Height : " + height + " Width : " + width);
+
+
+    }
 }
